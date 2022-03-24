9ed3e7aed552f377ac4579860cc0dc00bafde34a
kchia <kchia87@gmail.com>
2022-03-24 13:17:02 -0600

    update index.ts and create new scripts in package.json

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import { ApolloServer } from "apollo-server";
+import { ApolloServerPluginLandingPageLocalDefault } from "apollo-server-core";
 
 // tell Apollo Server which API operations to support in the GraphQL API
 import { schema } from "./schema";
@@ -7,9 +8,11 @@ import { context } from "./context";
 export const server = new ApolloServer({
   schema,
   context,
+  introspection: true, // so you can ask the GraphQL server what queries it supports; don't typically do in prod
+  plugins: [ApolloServerPluginLandingPageLocalDefault()], // enable Apollo sandbox; dont typically do in prod
 });
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 server.listen({ port }).then(({ url }) => {
   console.log(`🚀  Server ready at ${url}`);
 });
